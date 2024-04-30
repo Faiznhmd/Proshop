b60c80d967c6c3bfba2158ccdbc713d1e6933861
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-04-30 23:57:03 +0530

    data file on backend

--- src/screen/ProductScreen.jsx
+++ src/screen/ProductScreen.jsx
@@ -1,13 +1,21 @@
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-
-import products from '../product.js';
+import { Row, Col, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../component/Rating.jsx';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p.id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
