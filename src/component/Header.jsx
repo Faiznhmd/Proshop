b74e9c7d39b03d02c06128a2ccc21ec10d3c0a3f
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-04-30 12:00:39 +0530

    header

---
+++ src/component/Header.jsx
@@ -0,0 +1,29 @@
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart />
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser />
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
